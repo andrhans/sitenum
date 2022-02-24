e16688539ad3878707bb16968a49d342d8cdc654
andr <andreahm@gmx.com>
2022-02-24 12:15:34 -0500

    finalizing first draft

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Dir-enum is a directory enumeration tool.
 
 It is written in python.
+
+Working on a slightly more functional directory.
--- dir-enum.py
+++ dir-enum.py
@@ -4,3 +4,19 @@ import requests
 welcome_banner=pyfiglet.figlet_format("Port Scanner")
 print(welcome_banner)
 
+target=input("Enter target host: ")
+file=input("Enter wordlist location: ")
+
+def request(url):
+    try:
+        return requests.get("http://"+url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+wordlist=open(file, 'r')
+for line in wordlist:
+    dir=line.strip()
+    long_url=target+"/"+dir
+    response=request(long_url)
+    if response:
+        print("[*] Directory discovered: "+long_url)
